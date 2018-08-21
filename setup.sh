1d9e28d89f21ffd9d23a4314b0b8d31da08d75a6
Sha Liu <sandyleo26@gmail.com>
2018-08-22 03:50:40 +1000

    add readme

--- setup.sh
+++ setup.sh
@@ -20,5 +20,5 @@ make db-container && make db-container-start && sleep 3 && make db-migration
 echo "build and start api"
 make build-linux && make start-linux
 
-echo "Ready to go!"
-echo "try curl -X GET "
+echo "Ready to go! Try "
+echo "curl -X POST http://localhost:8080/order --data '{\"origin\": [\"1.335125\",\"103.76471334\"], \"destination\": [\"1.280776\",\"103.8487\"]}'"
